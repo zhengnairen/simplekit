902fbc3b52e2c49054298469ebfdbd2b71296a37
Nairen Zheng <zhengnairen@foxmail.com>
2021-09-26 04:05:39 +0800

    Fix bugs of reloading modules

--- __init__.py
+++ __init__.py
@@ -21,12 +21,29 @@ def reload_module(name, verbose=0):
 	for k, v in sys.modules.items():
 		if k.startswith(name):
 			v_list.append(v)
-	if verbose == 1:
+	if verbose > 0:
 		count_module = len(v_list)
 		unit = 's' if count_module > 1 else ''
 		print(f'reload {count_module} module{unit}')
+	count_fail = 0
+	module_list_fail = []
 	for i, v in enumerate(v_list):
 		if verbose == 2:
-			v_path = '; '.join(v.__path__)
-			print(f'[{i + 1}]: reload {v.__name__} from {v_path}')
-		importlib.reload(v)
+			try:
+				v_path = ' from ' + '; '.join(v.__path__)
+			except:
+				v_path = ''
+			print(f'[{i + 1}]: reload {v.__name__}{v_path}')
+		try:
+			importlib.reload(v)
+		except:
+			count_fail += 1
+			if verbose == 2:
+				module_list_fail.append(f'{v.__name__}{v_path}')
+	if count_fail:
+		if verbose > 0:
+			unit = 's' if count_fail > 1 else ''
+			print(f'{count_fail} module{unit} failed to reload')
+		if verbose == 2:
+			for i, v in enumerate(module_list_fail):
+				print(f'[{i + 1}]: {v}')
