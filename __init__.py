dffa6d01d290b9abfcd1bd94277f4a42186c51e5
Nairen Zheng <zhengnairen@foxmail.com>
2021-09-26 03:52:52 +0800

    Add a function to reload Python modules by name

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from .function import _handler
+import importlib
 import signal
 import sys
 
@@ -14,3 +15,18 @@ def exit_on_enter(timeout=5):
 	if enter is not None:
 		sys.exit(0)
 	signal.alarm(0)
+
+def reload_module(name, verbose=0):
+	v_list = []
+	for k, v in sys.modules.items():
+		if k.startswith(name):
+			v_list.append(v)
+	if verbose == 1:
+		count_module = len(v_list)
+		unit = 's' if count_module > 1 else ''
+		print(f'reload {count_module} module{unit}')
+	for i, v in enumerate(v_list):
+		if verbose == 2:
+			v_path = '; '.join(v.__path__)
+			print(f'[{i + 1}]: reload {v.__name__} from {v_path}')
+		importlib.reload(v)
