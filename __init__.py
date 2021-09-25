f6ad13067870cc06af02616998f0c815e66fef45
Nairen Zheng <zhengnairen@foxmail.com>
2021-09-26 02:46:10 +0800

    Add a function to exit if the user press ENTER

---
+++ __init__.py
@@ -0,0 +1,18 @@
+import signal
+import sys
+
+def handler(signum, frame):
+	raise RuntimeError
+
+def exit_on_enter(timeout=5):
+	signal.signal(signal.SIGALRM, handler)
+	print(f'Press ENTER in {timeout} seconds to exit')
+	enter = None
+	signal.alarm(timeout)
+	try:
+		enter = input()
+	except RuntimeError:
+		pass
+	if enter is not None:
+		sys.exit(0)
+	signal.alarm(0)
